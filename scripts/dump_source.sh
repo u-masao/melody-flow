a14d6e369abf8e84e536c303ad2d5a6f0d778b24
u-masao <4973920+u-masao@users.noreply.github.com>
2025-09-09 17:55:38 +0900

    ファイル名を変更

--- scripts/dump_source.sh
+++ scripts/dump_source.sh
@@ -10,9 +10,9 @@ src/model/chord_name_parser.py
 src/model/melody_processor.py
 src/api/main.py
 static/index.html
+static/app.html
 static/main.js
 static/presentation.html
-static/site.html
 )
 
 for ((i=0; i<${#FILES[@]}; i++))
